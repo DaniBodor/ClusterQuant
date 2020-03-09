c4393195e6966bfa2eb4f501b06a26f3296c19eb
bani <d.bodor@hubrecht.eu>
2020-03-09 14:48:55 +0100

    imports and data folder

--- src/CENS_per_square.py
+++ src/CENS_per_square.py
@@ -5,3 +5,20 @@ Created on Mon Mar  9 14:13:59 2020
 @author: dani
 """
 
+import pandas as pd
+import itertools
+import sys
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+from random import seed as rseed
+from pathlib import Path
+
+starttime = datetime.now()
+rseed(22)
+
+filename = 'Log_2003091404.txt'
+
+data = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'results', 'output', filename))
+
+print(data)
